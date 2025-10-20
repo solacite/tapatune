2a8094a13734a8a789a2874159d49549263ec357
solacite <julia.nguyen.do@gmail.com>
2025-10-20 19:12:44 -0400

    add tweens so it's smooth...smooth criminal...

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,19 +1,38 @@
 extends TextureButton
 
+var click_scale := Vector2(0.25, 0.25)
+var hover_scale := Vector2(0.3, 0.3)
+var normal_scale := Vector2(0.265, 0.265)
+
 var taps = 0
 var hover_angle = 15
+var tween
 
 func _ready() -> void:
 	await get_tree().process_frame
 	set_pivot_offset(size / 2)
 	position -= size / 2
+	scale = normal_scale
 
 func _on_pressed() -> void:
-	taps = taps + 1
+	scale = click_scale
+	taps += 1
 	$"../Taps".text = str(taps)
+	if tween:
+		tween.kill()
+	tween = create_tween()
+	tween.tween_property(self, "scale", normal_scale, 0.15)
 
 func _on_mouse_entered() -> void:
-	rotation_degrees = hover_angle
+	if tween:
+		tween.kill()
+	tween = create_tween()
+	tween.tween_property(self, "scale", hover_scale, 0.1)
+	tween.tween_property(self, "rotation_degrees", hover_angle, 0.1)
 
 func _on_mouse_exited() -> void:
-	rotation = 0
+	if tween:
+		tween.kill()
+	tween = create_tween()
+	tween.tween_property(self, "scale", normal_scale, 0.1)
+	tween.tween_property(self, "rotation_degrees", 0, 0.1)
