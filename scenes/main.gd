ad4777245beec12cf1d3a1cd67db614310f617c6
solacite <julia.nguyen.do@gmail.com>
2025-10-20 18:09:21 -0400

    still in the process of debugging WHY THE VINYL ISN'T ROTATING LIKE DOOOD IM NOT ASKING U TO RUN A MARATHON ??? T_T

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,18 +1,18 @@
-extends Node2D
+extends TextureButton
 
 var taps = 0
+var hover_angle = 0.8
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
-
-
 
 func _on_vinyl_pressed() -> void:
 	taps = taps + 1
-	$Taps.text = str(taps)
+	$"../Taps".text = str(taps)
+
+func _on_vinyl_mouse_entered() -> void:
+	print("HOVERING IS HAPPENING")
+	rotation_degrees = hover_angle
+
+func _on_vinyl_mouse_exited() -> void:
+	rotation = 0
